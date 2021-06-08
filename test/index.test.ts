889a1547b241eeccecd0bd4159df192c646ae46d
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2021-06-08 09:14:44 +0900

    update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,22 +1,18 @@
 import assert from 'assert';
 import { describe, it } from 'mocha';
-import http from 'http';
-const stripeStatefulMock = require('stripe-stateful-mock');
 import Stripe from "stripe";
 
-describe('Array', () => {
+describe('stripe', () => {
   const client = new Stripe("sk_test_foobar", {
       apiVersion: "2020-08-27",
       host: "localhost",
       port: 8000,
       protocol: "http"
   });
-  describe('#indexOf()', async () => {
-    it('should return -1 when the value is not present', () => {
-      assert.strictEqual([1, 2, 3].indexOf(4), -1);
-    });
-    it('stripe', async () => {
+  describe('#customers.create()', async () => {
+    it('email', async () => {
       const customer = await client.customers.create({email: 'customer@example.com'});
+      console.log(customer);
       assert.strictEqual(customer.email, 'customer@example.com');
     });
   });
